bb8361849c62a5dec6b895d00c148b7a1b4993a5
Kurt Russelle Marmol <jkurtrussellemarmol@gmail.com>
2025-06-19 17:43:01 +0800

    chore: install and configure Hero UI library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import StoreProvider from "@/app/store-provider";
+import { HeroUIProvider } from "@heroui/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <StoreProvider>{children}</StoreProvider>
+        <HeroUIProvider>
+          <StoreProvider>{children}</StoreProvider>
+        </HeroUIProvider>
       </body>
     </html>
   );
